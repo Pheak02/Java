33d2674764dc5df242687dd3a2ba20421a85d3a6
Pheak02 <email@gmail.com>
2023-01-08 18:58:00 +0700

    done

--- Ch2_Ex3.java
+++ Ch2_Ex3.java
@@ -1,9 +1,17 @@
-import java.util.Arrays;
-import java.util.Collection;
+//import java.util.Arrays;
+//import java.util.Collection;
+//public class Ch2_Ex3 {
+//    public static void main(String[] args){
+//        String[] arr = { "China, USA, LA, Thailand"};
+//        Array.sort(arr, Collections.reverseOrder());
+//        System.out.println(Array.toString(arr));
+//    }
+//}
 public class Ch2_Ex3 {
-    public static void main(String[] args){
-        String[] arr = { "China, USA, LA, Thailand"};
-        Array.sort(arr, Collections.reverseOrder());
-        System.out.println(Array.toString(arr));
+    int x = 5;
+
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        System.out.println(myObj.x);
     }
 }
