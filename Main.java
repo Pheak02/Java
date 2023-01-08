0d2399e44b4aad064a9a0433ad08fdd45f62b18a
Pheak02 <email@gmail.com>
2023-01-08 20:49:48 +0700

    complicated changes

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String [] args){
-        System.out.println("Hello World");
+import java.util.Arrays;
+public class Main
+{
+    public static void main(String[] args)
+    {
+//defining an array of integer type   
+        int [] array = new int [] {90, 23, 5, 109, 12, 22, 67, 34};
+//invoking sort() method of the Arrays class  
+        Arrays.sort(array);
+        System.out.println("Elements of array sorted in ascending order: ");
+//prints array using the for loop  
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.println(array[i]);
+        }
     }
-}
+}  
