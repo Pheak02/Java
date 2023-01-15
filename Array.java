6694e768ae82327c4964017d911057ca5950e002
Pheak02 <email@gmail.com>
2023-01-15 21:57:48 +0700

    Array

---
+++ Array.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        // Class in java - array
+
+        System.out.println(Arrays.toString(numbers));
+
+    }
+
+}
