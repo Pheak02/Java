a2cd3cd52632a6e8cf158d5d6d4cd16d5e8fb1d3
Pheak02 <email@gmail.com>
2023-01-15 22:41:09 +0700

    variable a

---
+++ Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String[] args) {
+        // ______Overwrite variable.________
+        // int age = 30;
+        // age = 35;
+        // System.out.println(age);
+
+        // ---------one var equal to another-------
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(myAge);
+    }
+
+}
